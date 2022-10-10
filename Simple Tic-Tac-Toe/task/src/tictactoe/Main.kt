f7a09b9ab64402ca8f98cdea38e9a6142f77a8f9
aprilte <apriltexs@gmail.com>
2022-10-11 00:17:41 +0900

    1011::1

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -3,8 +3,7 @@ package tictactoe
 fun main() {
     // write your code here
     val ttt = TTT()
-//  ttt.printfield()
-    ttt.demo()
+    ttt.updateByInput()
     ttt.printfield()
 }
 
@@ -14,12 +13,16 @@ class TTT() {
     val field = Array(row, {Array(col, {0})})
 
     fun printfield() {
+        println("---------")
         for (i in 0..field.lastIndex) {
+            print("| ")
             for (j in 0..field[0].lastIndex) {
                 printer(field[i][j])
             }
+            print("|")
             println()
         }
+        println("---------")
     }
 
     fun demo() {
@@ -34,9 +37,26 @@ class TTT() {
         field[2][2] = 1
     }
 
+    fun updateByInput() {
+        val inputMsg = readln()
+        var row = 0
+        var col = 0
+        inputMsg.toCharArray().withIndex().forEach {
+            row = it.index / 3
+            col = it.index % 3
+            field[row][col] =
+                when(it.value) {
+                    '_' -> 0
+                    'X' -> -1
+                    'O' -> 1
+                    else -> 0
+                }
+        }
+    }
+
     fun printer(i: Int) {
         if (i == 0) {
-            print("□ ")
+            print("_ ")
         } else if (i < 0) {
             print("X ")
         } else {
